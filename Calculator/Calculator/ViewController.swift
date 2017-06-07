bd26662bb8a55b87e45716b780ee8b852e336f90
Qwill Duvall <qwill.duvall@gmail.com>
2017-06-07 16:22:15 -0500

    Trying to sort out my git repo, I think this should have everything in calculator now

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Qwill Duvall on 6/6/17.
+//  Copyright © 2017 ThoughtWorks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTyping = false
+   
+    @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTyping {
+            let textCurrentlyInDisplay = display.text!
+            if !(textCurrentlyInDisplay.contains(".") && digit == ".") {
+                display.text = textCurrentlyInDisplay + digit
+            }
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTyping = true
+        }
+    }
+
+    var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
+    }
+    
+    private var brain = CalculatorBrain()
+    
+    @IBAction func performOperation(_ sender: UIButton) {
+        if userIsInTheMiddleOfTyping {
+            brain.setOperand(displayValue)
+            userIsInTheMiddleOfTyping = false
+        }
+        if let mathematicalSymbol = sender.currentTitle {
+            brain.performOperation(mathematicalSymbol)
+        }
+        if let result = brain.result {
+            displayValue = result
+        }
+        
+    }
+    
+}
+
