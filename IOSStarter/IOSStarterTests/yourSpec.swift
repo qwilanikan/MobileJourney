fb20aa913e512c6107099fbcadbb5d7a16377b9d
Qwill Duvall <qwill.duvall@gmail.com>
2017-06-14 17:15:49 -0500

    IOSStarter sets up testing with quick and nimble

--- IOSStarter/IOSStarterTests/yourSpec.swift
+++ IOSStarter/IOSStarterTests/yourSpec.swift
@@ -6,4 +6,17 @@
 //  Copyright © 2017 ThoughtWorks. All rights reserved.
 //
 
-import Foundation
+import Quick
+import Nimble
+
+class YourSpec: QuickSpec {
+    override func spec() {
+        describe("Your Spec") {
+            context("when this condition is met") {
+                it("should do this thing") {
+                    expect(true).to(beFalse())
+                }
+            }
+        }
+    }
+}
